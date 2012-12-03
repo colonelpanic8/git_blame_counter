25a68fdf943677278f2b01dd475adfd666fe4b63
Ivan Malison <IvanMalison@gmail.com>
2012-12-02 23:25:23 -0800

    Added committer blame line counting.

--- git_blame_counter.py
+++ git_blame_counter.py
@@ -5,6 +5,20 @@ import re
 import subprocess
 
 
+def segment(iterable, segment_length):
+	if segment_length is None:
+		yield iterable
+		raise StopIteration
+	def yield_length():
+		for _ in xrange(segment_length):
+			yield iterable.next()
+	while True:
+		segment = list(yield_length())
+		if not segment:
+			raise StopIteration
+		yield segment
+
+
 def build_file_extension_re(file_extensions):
 	return '.*\.(?:' + '|'.join(file_extensions) + ')'
 
@@ -12,18 +26,22 @@ def build_file_extension_re(file_extensions):
 class BlameCounter(object):
 
 	DIVIDER = '------------------------------'
+	commiter_matcher = re.compile('\((.*?)\s*[0-9]{4}')
 
 	def __init__(
 		self,
 		search_re='',
 		filename_re='.*\.(?:py|tmpl)',
-		directory_ignore_re=None
+		directory_ignore_re=None,
+		chunk_size=None,
+		committer=None
 	):
 		self.path_matcher = re.compile(search_re)
 		self.filename_matcher = re.compile(filename_re)
 		self.directory_ignore_matcher = re.compile(directory_ignore_re) \
 			if directory_ignore_re else None
-		self.commiter_matcher = re.compile('\((.*?)\s*[0-9]{4}')
+		self.chunk_size = chunk_size
+		self.committer = committer
 		self.blame_line_count_map = {}
 
 	def get_matching_files(self):
@@ -52,34 +70,50 @@ class BlameCounter(object):
 				stderr=subprocess.PIPE,
 			):
 				continue
-			yield subprocess.Popen(
+			yield (filename, subprocess.Popen(
 				['git', 'blame', filename],
 				stdout=subprocess.PIPE
-			).communicate()[0]
+			).communicate()[0])
 
-	def count_blame_lines_in_chunks(self, chunk_size):
-		for file_count, blame_output in enumerate(
-			self.git_blame_files(self.get_matching_files())
+	def count_blame_lines(self):
+		for blame_output_chunk in segment(
+			self.git_blame_files(self.get_matching_files()),
+			self.chunk_size
 		):
-			if file_count % chunk_size == 0:
+			self._count_blame_lines(blame_output_chunk)
+			if self.chunk_size:
 				self.print_results(
 					max_committers=50,
 					min_blame_lines=None
 				)
-			self._count_blame_lines(blame_output)
 
-	def count_blame_lines(self):
-		for blame_output in self.git_blame_files(self.get_matching_files()):
-			self._count_blame_lines(blame_output)
+	def _count_blame_lines(self, blame_outputs):
+		for _, blame_output in blame_outputs:
+			if self.committer and not self.committer in blame_output:
+				continue
+			for line in blame_output.split('\n'):
+				match = self.commiter_matcher.search(line)
+				if match:
+					committer = match.group(1)
+					self.blame_line_count_map[
+						committer
+					] = self.blame_line_count_map.setdefault(committer, 0) + 1
 
-	def _count_blame_lines(self, blame_output):
-		for line in blame_output.split('\n'):
-			match = self.commiter_matcher.search(line)
-			if match:
-				committer = match.group(1)
-				self.blame_line_count_map[
-					committer
-				] = self.blame_line_count_map.setdefault(committer, 0) + 1
+	def files_touched_by_comitter(self):
+		blame_count_by_file = {}
+		committer_matcher = re.compile('\((%s)\s*[0-9]{4}' % self.committer)
+		for filename, blame_output in self.git_blame_files(
+			self.get_matching_files()
+		):
+			if self.committer in blame_output:
+				count = 0
+				for line in blame_output.split('\n'):
+					match = committer_matcher.search(line)
+					if match:
+						count +=1
+				blame_count_by_file[filename] = count
+				print filename, count
+		return blame_count_by_file
 
 	def print_results(self, max_committers=None, min_blame_lines=None):
 		print self.DIVIDER
@@ -108,8 +142,7 @@ if __name__ == '__main__':
 		action='append',
 		dest='file_extensions',
 		help=('Search for filenames with the given file extension. '
-			  'Can be used multiple times.'
-		)
+			  'Can be used multiple times.')
 	)
 	parser.add_option(
 		'--chunk-size',
@@ -117,10 +150,19 @@ if __name__ == '__main__':
 		type=int,
 		help='Print the rankings at intervals of CHUNK_SIZE files.'
 	)
+	parser.add_option(
+		'--committer',
+		dest='committer',
+		help=('Count blame lines only in files where COMMITTER appears. '
+			  'Overrides --chunk-size.')
+	)
 
 	(namespace, _) = parser.parse_args()
 
-	blame_counter_build_kwargs = {}
+	blame_counter_build_kwargs = {
+		'committer': namespace.committer,
+		'chunk_size': namespace.chunk_size
+	}
 	if namespace.file_extensions:
 		blame_counter_build_kwargs['filename_re'] = build_file_extension_re(
 			namespace.file_extensions
@@ -129,10 +171,5 @@ if __name__ == '__main__':
 		blame_counter_build_kwargs['search_re'] = namespace.search_re
 
 	blame_counter = BlameCounter(**blame_counter_build_kwargs)
-
-	if namespace.chunk_size:
-		blame_counter.count_blame_lines_in_chunks(namespace.chunk_size)
-	else:
-		blame_counter.count_blame_lines()
-
+	blame_counter.count_blame_lines()
 	blame_counter.print_results()
